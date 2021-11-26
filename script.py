6db2a53f492890c6ede62448fa23bb779f109317
thunderteammax <94621146+thunderteammax@users.noreply.github.com>
2021-11-26 14:53:13 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,19 +1,20 @@
 class script(object):
-    START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
+    START_TEXT = """ Hey there my name is Thunder GoFile A Simple File Renamer Bot With Permanent Thumbnail support!
+
 
 <b>Send me any Telegram file and choose appropriate option! </b>"""
 
-    RENAME_403_ERR = "What Are You Doing? You are Banned"
-    UPGRADE_TEXT = "CONTACT @prgofficial"
+    RENAME_403_ERR = "I Fell into Error Contact our Supporter"
+    UPGRADE_TEXT = "CONTACT @thunderbotcontact"
     DOWNLOAD_START = "Give Me Some Time..."
     UPLOAD_START = "Starting to upload..."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  Thunder Team **"
     SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
     DEL_THUMB = "Thumbnail cleared succesfully!"
     NO_THUMB = "No thumbnails found!"
-    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully "
     CUSTOM_CAPTION_UL_FILE = " "
-    HELP_USER = """It's not that complicated😅
+    HELP_USER = """It's not that complicated
     
 1. Send me any Telegram File.
 2. Choose appropriate option."""
